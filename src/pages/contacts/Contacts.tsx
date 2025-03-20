9f2e1f354cce56e94f2cbd20f364ebc44a60b2e1
Razmik <razmik.unanyan@mail.ru>
2025-03-20 09:59:31 -0500

    update contacts

--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -9,28 +9,28 @@ import { Button, Input, PageTitle, Shapes, Textarea } from "../../components";
 export const Contacts: FC<IContactsProps> = () => {
   return (
     <motion.section className={styles.contacts_wrapper} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-      <PageTitle>Контакты</PageTitle>
+      <PageTitle>Contacts</PageTitle>
       <div className={styles.contacts}>
         <div className={styles.contacts_info}>
-          <h3 className={styles.contacts_title}>Остались Вопросы?</h3>
-          <p className={styles.contacts_Detail}>Наш менеджер свяжется с вами.</p>
+          {/*<h3 className={styles.contacts_title}>Остались Вопросы?</h3>*/}
+          {/*<p className={styles.contacts_Detail}>Наш менеджер свяжется с вами.</p>*/}
         </div>
         <form action="" className={styles.contacts_form}>
           <div className={styles.contacts_form_group}>
             <div className={styles.contacts_form_input_wrapper}>
-              <Input type="text" placeholder="Имя" />
+              <Input type="text" placeholder="Name" />
             </div>
             <div className={styles.contacts_form_input_wrapper}>
               <Input type="text" placeholder="Email" />
             </div>
           </div>
           <div className={styles.contacts_form_input_wrapper}>
-            <Input type="text" placeholder="Тема" />
+            <Input type="text" placeholder="Subject" />
           </div>
           <div className={cn(styles.contacts_form_input_wrapper, styles.contacts_form_input_area)}>
-            <Textarea placeholder="Текст" name="" id="" rows={30} cols={10}></Textarea>
+            <Textarea placeholder="Text" name="" id="" rows={30} cols={10}></Textarea>
           </div>
-          <Button>Отправить</Button>
+          <Button>Send</Button>
         </form>
       </div>
       <Shapes />
